97f45cd193f4c43f247e6bf634249dd3937c8975
CodeWithChi3fkabbz <82002738+chiefkabb@users.noreply.github.com>
2023-03-13 14:44:52 +0000

    Add files via upload

---
+++ I Dont Have P's/I Dont Have P's/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I Dont Have P's
+//
+//  Created by Suffian Kabba on 05/11/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
